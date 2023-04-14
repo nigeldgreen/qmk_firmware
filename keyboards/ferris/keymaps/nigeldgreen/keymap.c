91424b04ba31fb78187a7cd09baee97cba9ca75d
nigeldgreen <nigel@greenlemur.com>
2023-04-14 13:13:49 +0100

    updates combos for esc, tab and enter

--- keyboards/ferris/keymaps/nigeldgreen/keymap.c
+++ keyboards/ferris/keymaps/nigeldgreen/keymap.c
@@ -10,12 +10,14 @@ enum ferris_layers {
   _FUN,
 };
 
-#define COMBO_COUNT 15
+#define COMBO_COUNT 17
 uint16_t COMBO_LEN = COMBO_COUNT;
 
-const uint16_t PROGMEM cmb_esc[] = {KC_W, KC_E, COMBO_END};
-const uint16_t PROGMEM cmb_tab[] = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM cmb_ent[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM cmb_esc[] = {KC_S, KC_D, COMBO_END};
+const uint16_t PROGMEM cmb_tab[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM cmb_lent[] = {KC_S, KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM cmb_rent[] = {KC_J, KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM cmb_del[] = {KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM cmb_caps_word[] = {KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM cmb_copy[] = {KC_F, KC_G, COMBO_END};
 const uint16_t PROGMEM cmb_paste[] = {KC_V, KC_B, COMBO_END};
@@ -30,8 +32,10 @@ const uint16_t PROGMEM cmb_lbrc[] = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM cmb_rbrc[] = {KC_COMM, KC_DOT, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(cmb_tab, KC_TAB),
-    COMBO(cmb_ent, KC_ENT),
+    COMBO(cmb_lent, KC_ENT),
+    COMBO(cmb_rent, KC_ENT),
     COMBO(cmb_esc, KC_ESC),
+    COMBO(cmb_del, KC_DEL),
     COMBO(cmb_caps_word, CW_TOGG),
     COMBO(cmb_copy, C(KC_C)),
     COMBO(cmb_paste, C(KC_V)),
@@ -72,37 +76,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWER] = LAYOUT(
     KC_Q,      KC_W,      KC_E,     KC_R,       KC_T,              KC_Y,      KC_U,       KC_I,    KC_O,     KC_P,
     KC_A,      KC_S,      KC_D,     KC_F,       KC_G,              KC_J,      MO(_SYM2),  KC_K,    KC_L,     KC_SCLN,
-    KC_Z,      KC_X,      KC_C,     KC_V,       KC_B,              KC_M,      KC_COMM,    KC_SPC,  KC_RSFT,  KC_H,
+    KC_Z,      KC_X,      KC_C,     KC_V,       KC_B,              KC_M,      KC_COMM,    KC_SPC,  KC_NO,  KC_H,
                                 MO(_NUM),  MO(_SYM1),              KC_DOT,    KC_N
   ),
   [_CMAK] = LAYOUT(
     KC_Q,      KC_W,      KC_F,      KC_P,      KC_B,              KC_J,      KC_L,       KC_U,    KC_Y,     KC_SCLN,
     KC_A,      KC_R,      KC_S,      KC_T,      KC_G,              KC_N,      MO(_SYM2),  KC_E,    KC_I,     KC_O,
-    KC_Z,      KC_X,      KC_C,      KC_D,      KC_V,              KC_H,      KC_COMM,    KC_SPC,  KC_RSFT,  KC_M,
+    KC_Z,      KC_X,      KC_C,      KC_D,      KC_V,              KC_H,      KC_COMM,    KC_SPC,  KC_NO,    KC_M,
                                  MO(_NUM), MO(_SYM1),              KC_DOT,    KC_K
   ),
   [_SYM1] = LAYOUT(
     KC_NO,    KC_NO,   _MVSCRT,   _SHSCRT,     KC_NO,              KC_NO,     _UNDSCR,    KC_MINS, KC_EQL,   KC_PAST,
     _LSFT,     _ALT,      _GUI,     _LCTL,      _MEH,              KC_QUOT,   MO(_FUN),   S(KC_2), KC_CIRC,  C(KC_6),
-    KC_NO,    KC_NO,     KC_NO,     _SGUI,     KC_NO,              _TILDE,    KC_SLSH,    KC_BSPC, KC_TRNS,  KC_NO,
+    KC_NO,    KC_NO,     KC_NO,     KC_NO,     KC_NO,              _TILDE,    KC_SLSH,    KC_BSPC, KC_NO,    KC_NO,
                                     KC_NO,   KC_TRNS,              KC_NUBS,   KC_NO
   ),
   [_SYM2] = LAYOUT(
      KC_GRV,  KC_DLR,  KC_PLUS,   KC_NUHS,     KC_NO,              KC_NO,     KC_NO,      KC_NO,   KC_NO,    KC_NO,
     S(KC_3), KC_PERC,  KC_EXLM,   KC_QUES,     KC_NO,              _LCTL,     KC_TRNS,    _GUI,    _ALT,     _LSFT,
-    KC_TRNS, KC_AMPR,    _PIPE,   _ATSIGN,     KC_NO,              KC_NO,     KC_NO,      KC_NO,   KC_NO,      _MEH,
+    KC_TRNS, KC_AMPR,    _PIPE,   _ATSIGN,     KC_NO,              KC_NO,     KC_NO,      KC_NO,   KC_NO,    _MEH,
                                  MO(_NAV),     KC_NO,              KC_NO,     KC_NO
   ),
   [_NUM] = LAYOUT(
       KC_NO,   KC_NO,    KC_NO,     KC_NO,     KC_NO,             KC_LEFT,    KC_7,       KC_8,    KC_9,     KC_RIGHT,
       _LSFT,    _ALT,     _GUI,     _LCTL,      _MEH,             KC_4,       KC_BSPC,    KC_5,    KC_6,     KC_NO,
-    KC_TRNS,   KC_NO,    KC_NO,     KC_NO,    KC_DEL,             KC_1,       KC_2,       KC_0,    KC_TRNS,  KC_NO,
+    KC_TRNS,   KC_NO,    KC_NO,     KC_NO,    KC_DEL,             KC_1,       KC_2,       KC_0,    KC_NO,    KC_NO,
                                   KC_TRNS,     KC_NO,             KC_3,       KC_NO
   ),
   [_NAV] = LAYOUT(
       KC_NO,   KC_NO,    KC_NO,     KC_NO,     KC_NO,              KC_PGUP,   KC_HOME,    KC_NO,   KC_NO,    KC_NO,
       _LSFT,    _ALT,     _GUI,     _LCTL,      _MEH,              KC_DOWN,   KC_NO,      KC_UP,   KC_RIGHT, KC_NO,
-    KC_TRNS,   KC_NO,    _SCTL,     _SGUI,     KC_NO,              KC_END,    KC_NO,      KC_NO,   KC_TRNS,  KC_LEFT,
+    KC_TRNS,   KC_NO,    KC_NO,     KC_NO,     KC_NO,              KC_END,    KC_NO,      KC_NO,   KC_NO,    KC_LEFT,
                                   KC_TRNS,     KC_NO,              KC_NO,     KC_PGDN
   ),
   [_FUN] = LAYOUT(
