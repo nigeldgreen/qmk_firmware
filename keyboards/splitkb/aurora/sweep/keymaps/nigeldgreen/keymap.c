28bddf5eab75b2511e0440d98d04bbc9c89df48c
Nigel Green <nigel@greenlemur.com>
2023-05-22 07:53:48 +0100

    update keymap and add I3 layer back as one-shot

--- keyboards/splitkb/aurora/sweep/keymaps/nigeldgreen/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/nigeldgreen/keymap.c
@@ -7,48 +7,112 @@ enum ferris_layers {
   _NUM,
   _I3,
 };
-#define _MEH OSM(MOD_MEH)
-#define _ALT OSM(MOD_LALT)
-#define _SALT OSM(MOD_LSFT | MOD_LALT)
-#define _LCTL OSM(MOD_LCTL)
-#define _SCTL OSM(MOD_LSFT | MOD_LCTL)
-#define _GUI OSM(MOD_LGUI)
-#define _SGUI OSM(MOD_LSFT | MOD_LGUI)
-#define _CTLSPC C(KC_SPC)
+
+/* Define our QWERTY home row mods */
+#define _ALT_A ALT_T(KC_A)
+#define _GUI_S GUI_T(KC_S)
+#define _SFT_D SFT_T(KC_D)
+#define _CTL_F CTL_T(KC_F)
+#define _CTL_J CTL_T(KC_J)
+#define _SFT_K SFT_T(KC_K)
+#define _GUI_L GUI_T(KC_L)
+#define _ALT_SC ALT_T(KC_SCLN)
+
+/* Define our Colemak home row mods */
+#define _GUI_R GUI_T(KC_R)
+#define _SFT_S SFT_T(KC_S)
+#define _CTL_T CTL_T(KC_T)
+#define _CTL_N CTL_T(KC_N)
+#define _SFT_E SFT_T(KC_E)
+#define _GUI_I GUI_T(KC_I)
+#define _ALT_O ALT_T(KC_O)
+
+/* Define our layer switching keys */
+#define _SYM_TAB LT(_SYM, KC_TAB)
+#define _NUM_ESC LT(_NUM, KC_ESC)
+#define _NUM_ENT LT(_NUM, KC_ENT)
+#define _SYM_SPC LT(_SYM, KC_SPC)
+
+/* Define some character keys to keep letter count low */
 #define _PIPE S(KC_NUBS)
 #define _ATSIGN S(KC_QUOT)
 #define _UNDSCR S(KC_MINS)
 #define _TILDE S(KC_NUHS)
-#define _I3SHFT LM(_I3, MOD_LSFT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWER] = LAYOUT(
-       KC_Q,   KC_W,      KC_E,     KC_R,        KC_T,             KC_Y,      KC_U,       KC_I,    KC_O,     KC_P,
-ALT_T(KC_A),GUI_T(KC_S),SFT_T(KC_D),CTL_T(KC_F), KC_G,             KC_H,CTL_T(KC_J),SFT_T(KC_K),GUI_T(KC_L),ALT_T(KC_SCLN),
-SFT_T(KC_Z),   KC_X,      KC_C,     KC_V,        KC_B,             KC_N,      KC_M,       KC_COMM, KC_DOT,   KC_RSFT,
-                 LT(_SYM, KC_TAB),  LT(_NUM, KC_ESC),             LT(_NUM, KC_ENT),  LT(_SYM, KC_SPC)
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | Q         | W         | E         | R         | T         |   | Y         | U         | I         | O         | P         |
+      KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,           KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | A         | S         | D         | F         | G         |   | H         | J         | K         | L         | ;         |
+      _ALT_A,     _GUI_S,     _SFT_D,     _CTL_F,     KC_G,           KC_H,       _CTL_J,     _SFT_K,     _GUI_L,     _ALT_SC,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | Z         | X         | C         | V         | B         |   | N         | M         | ,         | .         | > _I3     |
+      KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,           KC_N,       KC_M,       KC_COMM,    KC_DOT,     OSL(_I3),
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//                                      | > _SYM    | > _NUM    |   | > _NUM    | > _SYM    |
+                                          MO(_SYM),   MO(_NUM),       MO(_NUM),   MO(_SYM)
+//                                      |-----------|-----------|   |-----------|-----------|
   ),
   [_CMAK] = LAYOUT(
-       KC_Q,   KC_W,      KC_F,     KC_P,        KC_B,             KC_J,      KC_L,      KC_U,     KC_Y,     KC_SCLN,
-       KC_A,   KC_R,      KC_S,     KC_T,        KC_G,             KC_M,      KC_N,      KC_E,     KC_I,     KC_O,
-SFT_T(KC_Z),   KC_X,      KC_C,     KC_D,        KC_V,             KC_K,      KC_H,      KC_COMM,  KC_DOT,   KC_RSFT,
-                 LT(_SYM, KC_TAB),  LT(_NUM, KC_ESC),             LT(_NUM, KC_ENT),  LT(_SYM, KC_SPC)
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | Q         | W         | F         | P         | B         |   | J         | L         | U         | Y         | ;         |
+      KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,           KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | A         | R         | S         | T         | G         |   | M         | N         | E         | I         | O         |
+      _ALT_A,     _GUI_R,     _SFT_S,     _CTL_T,     KC_G,           KC_M,       _CTL_N,     _SFT_E,     _GUI_I,     _ALT_O,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | Z         | X         | C         | D         | V         |   | K         | H         | ,         | .         | --        |
+      KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,           KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_NO,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//                                      | > _SYM    | > _NUM    |   | > _NUM    | > _SYM    |
+                                          MO(_SYM),   MO(_NUM),       MO(_NUM),   MO(_SYM)
+//                                      |-----------|-----------|   |-----------|-----------|
   ),
   [_SYM] = LAYOUT(
-     KC_GRV, KC_PLUS,  KC_NUHS,   KC_LCBR,    KC_RCBR,             KC_DLR,   _UNDSCR,    KC_MINS,  KC_EQL,   KC_QUOT,
-    KC_NUBS, KC_PAST,  KC_EXLM,   KC_LPRN,    KC_RPRN,             KC_LEFT,   KC_DOWN,   KC_UP,    KC_RIGHT, S(KC_2),
-    S(KC_3), KC_AMPR,  _ATSIGN,   KC_LBRC,    KC_RBRC,             KC_PERC,   _TILDE,    KC_SLSH,  KC_QUES,  _PIPE,
-                                  MO(_I3),    _I3SHFT,             KC_DEL,    KC_BSPC
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | `         | +         | #         | {         | }         |   | $         | _         | -         | =         | '         |
+      KC_GRV,    KC_PLUS,     KC_NUHS,    KC_LCBR,    KC_RCBR,        KC_DLR,     _UNDSCR,    KC_MINS,    KC_EQL,     KC_QUOT,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | \         | *         | !         | (         | )         |   | LEFT      | DOWN      | UP        | RIGHT     | "         |
+      KC_NUBS,    KC_PAST,    KC_EXLM,    KC_LPRN,    KC_RPRN,        KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   S(KC_2),
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | £         | &         | @         | [         | ]         |   | %         | ~         | /         | ?         | |         |
+      S(KC_3),    KC_AMPR,    _ATSIGN,    KC_LBRC,    KC_RBRC,        KC_PERC,    _TILDE,     KC_SLSH,    KC_QUES,    _PIPE,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//                                      | --        | --        |   | DEL       | BSPC      |
+                                          KC_NO,      KC_NO,          KC_DEL,     KC_BSPC
+//                                      |-----------|-----------|   |-----------|-----------|
   ),
   [_NUM] = LAYOUT(
-      KC_NO,   KC_F7,    KC_F8,     KC_F9,     KC_F10,             KC_PGUP,   KC_7,      KC_8,     KC_9,     KC_HOME,
-      KC_NO,   KC_F4,    KC_F5,     KC_F6,     KC_F11,             KC_NO,     KC_4,      KC_5,     KC_6,     KC_END,
-    KC_TRNS,   KC_F1,    KC_F2,     KC_F3,     KC_F12,             KC_PGDN,   KC_1,      KC_2,     KC_3,     KC_TRNS,
-                                    KC_NO,    KC_TRNS,             KC_BSPC,   KC_0
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | --        | F7        | F8        | F9        | F10       |   | PAGE UP   | 7         | 8         | 9         | HOME      |
+      KC_NO,      KC_F7,      KC_F8,      KC_F9,      KC_F10,         KC_PGUP,    KC_7,       KC_8,       KC_9,       KC_HOME,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | KB_BOOT   | F4        | F5        | F6        | F11       |   | --        | 4         | 5         | 6         | END       |
+      QK_BOOT,    KC_F4,      KC_F5,      KC_F6,      KC_F11,         KC_NO,      KC_4,       KC_5,       KC_6,       KC_END,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | --        | F1        | F2        | F3        | F12       |   | PAGE DOWN | 1         | 2         | 3         | --        |
+      KC_NO,      KC_F1,      KC_F2,      KC_F3,      KC_F12,         KC_PGDN,    KC_1,       KC_2,       KC_3,       KC_NO,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//                                      | --        | --        |   | BSPC      | 0         |
+                                          KC_NO,      KC_NO,          KC_BSPC,    KC_0
+//                                      |-----------|-----------|   |-----------|-----------|
   ),
   [_I3] = LAYOUT(
-    G(KC_Q), G(KC_W),  G(KC_E),   G(KC_R),    QK_BOOT,             G(KC_MINS), G(KC_7),  G(KC_8),  G(KC_9),  KC_NO,
-      KC_NO,   KC_NO,    KC_NO,     KC_NO,      KC_NO,             G(KC_LT),   G(KC_4),  G(KC_5),  G(KC_6),  KC_NO,
-    KC_TRNS,   KC_NO,    KC_NO,     KC_NO,      KC_NO,             KC_NO,      G(KC_1),  G(KC_2),  G(KC_3),  KC_TRNS,
-                                  KC_TRNS,    KC_TRNS,             KC_NO,      G(KC_0)
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | --        | --        | --        | --        | --        |   | --        | WS 7      | WS 8      | WS 9      | --        |
+      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      G(KC_7),    G(KC_8),    G(KC_9),    KC_NO,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | --        | --        | --        | --        | --        |   | --        | WS 4      | WS 5      | WS 6      | --        |
+      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      G(KC_4),    G(KC_5),    G(KC_6),    KC_NO,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//  | --        | --        | --        | --        | --        |   | --        | WS 1      | WS 2      | WS 3      | --        |
+      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      G(KC_1),    G(KC_2),    G(KC_3),    KC_NO,
+//  |-----------|-----------|-----------|-----------|-----------|   |-----------|-----------|-----------|-----------|-----------|
+//                                      | --        | --        |   | --        | --        |
+                                          KC_NO,      KC_NO,          KC_NO,      KC_NO
+//                                      |-----------|-----------|   |-----------|-----------|
   ),
 };
