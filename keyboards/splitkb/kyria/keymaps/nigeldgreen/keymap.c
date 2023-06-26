c749eff45836e671a15cf7b6fae95059882f47c6
Nigel Green <nigel@greenlemur.com>
2023-06-26 09:30:01 +0100

    initial kyria map complete

--- keyboards/splitkb/kyria/keymaps/nigeldgreen/keymap.c
+++ keyboards/splitkb/kyria/keymaps/nigeldgreen/keymap.c
@@ -107,49 +107,48 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
      KC_NO,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                          KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_NO,
      KC_NO,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                          KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_NO,
-     KC_NO,  _SFT_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_RSFT, KC_NO,
-                                 KC_Z,   MO(6), _SYMTAB, _NUMESC, KC_QUOT,      KC_NO,   _FUNENT, _SYMSPC,  MO(6),    QK_LEAD
+     KC_NO,  _SFT_Z,    KC_X,    KC_C,    KC_V,    KC_B,   _LCTL,   KC_NO,      KC_NO,   _LCTL,   KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_RSFT, KC_NO,
+                                 KC_Z,   MO(6), _SYMTAB, _NUMESC, KC_QUOT,     S(KC_2),  _FUNENT, _SYMSPC,  MO(6),    QK_LEAD
   ),
   [1] = LAYOUT(
      KC_NO,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                          KC_J,     KC_L,     KC_U,    KC_Y,    KC_SCLN, KC_NO,
      KC_NO,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                          KC_M,     KC_N,     KC_E,    KC_I,    KC_O,    KC_NO,
-     KC_NO,  _SFT_Z,    KC_X,    KC_C,    KC_D,    KC_V,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_K,     KC_H,     KC_COMM, KC_DOT,  KC_RSFT, KC_NO,
-                                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO
+     KC_NO,  _SFT_Z,    KC_X,    KC_C,    KC_D,    KC_V,   _LCTL,   KC_NO,      KC_NO,   _LCTL,   KC_K,     KC_H,     KC_COMM, KC_DOT,  KC_RSFT, KC_NO,
+                                 KC_Z,   MO(6), _SYMTAB, _NUMESC, KC_QUOT,     S(KC_2),  _FUNENT, _SYMSPC,  MO(6),    QK_LEAD
   ),
   [2] = LAYOUT(
      KC_NO,  KC_GRV, KC_PERC, KC_NUHS, KC_AMPR, KC_PLUS,                                          KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
      KC_NO, KC_NUBS, KC_PAST, KC_EXLM, KC_QUOT, S(KC_2),                                          C(KC_F),  _LCTL,    _GUI,    _ALT,    _MEH,    KC_NO,
-     KC_NO, S(KC_3),   _PIPE, _ATSIGN,  KC_DLR,  _LOREM,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    _SCTL,    _SGUI,   _SALT,   KC_TRNS, KC_NO,
-                                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO
+     KC_NO, S(KC_3),   _PIPE, _ATSIGN,  KC_DLR,  _LOREM, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_NO,    _SCTL,    _SGUI,   _SALT,   KC_TRNS, KC_NO,
+                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
   [3] = LAYOUT(
      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                          KC_PGUP,  _UNDSCR,  KC_MINS, KC_EQL,   KC_NO,  KC_NO,
      KC_NO,    _MEH,    _ALT,    _GUI,   _LCTL, C(KC_F),                                          KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, KC_NO,  KC_NO,
-     KC_NO,   KC_NO,   _SALT,   _SGUI,   _SCTL,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_PGDN,  _TILDE,   KC_SLSH, KC_QUES,  KC_NO,  KC_NO,
-                                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO
+     KC_NO,   KC_NO,   _SALT,   _SGUI,   _SCTL,   KC_NO, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_PGDN,  _TILDE,   KC_SLSH, KC_QUES,  KC_NO,  KC_NO,
+                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_DEL,  KC_BSPC,  KC_TRNS,  KC_TRNS
   ),
   [4] = LAYOUT(
      KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,    KC_NO,                                          KC_NO,    KC_7,     KC_8,    KC_9,    KC_NO,   KC_NO,
      KC_NO,    _MEH,    _ALT,    _GUI,   _LCTL, C(KC_F),                                          KC_NO,    KC_4,     KC_5,    KC_6,    KC_NO,   KC_NO,
-     KC_NO,   KC_NO,   _SALT,   _SGUI,   _SCTL,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    KC_1,     KC_2,    KC_3,    KC_NO,   KC_NO,
-                                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO
+     KC_NO,   KC_NO,   _SALT,   _SGUI,   _SCTL,   KC_NO, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_NO,    KC_1,     KC_2,    KC_3,    KC_NO,   KC_NO,
+                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_0,  KC_TRNS,  KC_TRNS
   ),
   [5] = LAYOUT(
      KC_NO,   KC_NO,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                                          KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
      KC_NO, QK_BOOT,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                                          C(KC_F),  _LCTL,    _GUI,    _ALT,    _MEH,    KC_NO,
-     KC_NO,   KC_NO,   KC_F1,   KC_F2,   KC_F3,  KC_F12,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    _SCTL,    _SGUI,   _SALT,   KC_TRNS, KC_NO,
-                                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO
+     KC_NO,   KC_NO,   KC_F1,   KC_F2,   KC_F3,  KC_F12, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_NO,    _SCTL,    _SGUI,   _SALT,   KC_TRNS, KC_NO,
+                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
   [6] = LAYOUT(
      KC_NO, _I3QUIT, _I3TABS, G(KC_E),  _I3RST,   KC_NO,                                          KC_NO,    G(KC_7),  G(KC_8), G(KC_9), _I3ASCR, KC_NO,
      KC_NO,   KC_NO, _I3STCK,   KC_NO, KC_LSFT,   KC_NO,                                          _I3MV,    G(KC_4),  G(KC_5), G(KC_6),  _I3SCR, KC_NO,
-     KC_NO,   KC_NO, G(KC_X),   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    G(KC_1),  G(KC_2), G(KC_3), _I3RSCR, KC_NO,
-                                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO
+     KC_NO,   KC_NO, G(KC_X),   KC_NO,   KC_NO,   KC_NO, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_NO,    G(KC_1),  G(KC_2), G(KC_3), _I3RSCR, KC_NO,
+                              KC_TRNS, KC_TRNS, G(KC_TAB), KC_TRNS, KC_TRNS,      KC_TRNS, G(KC_0), G(KC_SPC), KC_TRNS,  KC_TRNS
   )
 };
 
