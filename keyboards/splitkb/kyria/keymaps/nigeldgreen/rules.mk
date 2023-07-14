ceab56a508743bbe6b070488aad1d5fd14ca9746
nigeldgreen <nigel@greenlemur.com>
2023-07-14 17:04:46 +0100

    updates for tmux and FF tab switching

--- keyboards/splitkb/kyria/keymaps/nigeldgreen/rules.mk
+++ keyboards/splitkb/kyria/keymaps/nigeldgreen/rules.mk
@@ -1,6 +1,4 @@
-# MOUSEKEY_ENABLE = yes    # Mouse keys
-# RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-# COMBO_ENABLE    = yes
-# LEADER_ENABLE    = yes
+COMBO_ENABLE    = yes
+LEADER_ENABLE   = yes
 LTO_ENABLE      = yes
 
