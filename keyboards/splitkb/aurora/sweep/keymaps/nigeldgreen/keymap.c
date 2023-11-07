eb46ef44920ba2880b417a59de3aa584f7f0584f
nigeldgreen <nigel@greenlemur.com>
2023-11-07 20:35:33 +0000

    work machine changes

--- keyboards/splitkb/aurora/sweep/keymaps/nigeldgreen/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/nigeldgreen/keymap.c
@@ -75,8 +75,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,     KC_SCLN,
-         KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,    KC_N,    KC_E,    KC_I,     KC_O,
+         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_O,     KC_Y,
+         KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,    KC_N,    KC_E,    KC_I,     KC_SCLN,
       SC_LSPO,    KC_X,    KC_C,    KC_D,    KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,   SC_RSPC,
                                   SYMTAB,  NUMESC,        FUNENT,  SYMSPC
   ),
@@ -87,21 +87,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   SYMTAB,  NUMESC,        FUNENT,  SYMSPC
   ),
   [2] = LAYOUT(
-       KC_GRV, KC_PERC, KC_AMPR, KC_NUHS, KC_PLUS,        KC_NO,    KC_NO,    KC_NO,   KC_NO,    KC_NO,
+       KC_GRV, KC_PERC, KC_AMPR, KC_NUHS, KC_PLUS,        KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_NO,
       KC_NUBS, KC_PAST, KC_EXLM, KC_QUOT, S(KC_2),        KC_NO,   _LCTL,   _GUI,    _ALT,     _MEH,
          KC_Z,  KC_DLR,  ATSIGN, KC_LBRC, KC_RBRC,        KC_NO,   _SCTL,   _SGUI,   _SALT,    KC_TRNS,
                                    KC_NO,   KC_NO,        KC_NO,   KC_TRNS
   ),
   [3] = LAYOUT(
-        DF(0),  KC_SPC, KC_BSPC,  KC_DEL,  KC_ENT,        KC_PGUP, UNDSCR, KC_MINS, KC_EQL,   KC_HOME,
-        DF(1),    _ALT,    _GUI,   _LCTL,  TMXPRV,        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_END,
-      KC_TRNS,   _SALT,   _SGUI,   _SCTL,  TMXNXT,        KC_PGDN, TILDE,  KC_SLSH, KC_QUES,  CW_TOGG,
+        DF(0),  KC_SPC, KC_BSPC,  KC_DEL,  KC_ENT,        KC_HOME, UNDSCR, KC_MINS, KC_EQL,   CW_TOGG,
+        DF(1),    _ALT,    _GUI,   _LCTL,  TMXPRV,        KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, S(KC_SCLN),
+      KC_TRNS,   _SALT,   _SGUI,   _SCTL,  TMXNXT,        KC_END,  TILDE,  KC_SLSH, KC_QUES,  KC_NO,
                                  KC_TRNS,   KC_NO,        KC_DEL,  KC_BSPC
   ),
   [4] = LAYOUT(
-        KC_F5,   KC_F7,   KC_F8,   KC_F9,  KC_F10,        KC_NO,   KC_7,     KC_8,    KC_9,     KC_MINS,
+        KC_F5,   KC_F7,   KC_F8,   KC_F9,  KC_F10,        KC_PGUP, KC_7,     KC_8,    KC_9,     KC_MINS,
          _MEH,    _ALT,    _GUI,   _LCTL,  KC_F12,        KC_NO,   KC_4,     KC_5,    KC_6,     COMSPC,
-      KC_TRNS,   _SALT,   _SGUI,   _SCTL,   KC_F2,        KC_NO,   KC_1,     KC_2,    KC_3,     KC_PLUS,
+      KC_TRNS,   _SALT,   _SGUI,   _SCTL,   KC_F2,        KC_PGDN, KC_1,     KC_2,    KC_3,     KC_PLUS,
                                    KC_NO, KC_TRNS,        KC_BSPC, KC_0
   ),
   [5] = LAYOUT(
@@ -111,9 +111,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    MO(6),   KC_NO,        KC_TRNS, KC_NO
   ),
   [6] = LAYOUT(
-     S(G(KC_Q)), G(KC_W), G(KC_E), S(G(KC_R)), KC_NO,     G(KC_0), G(KC_7), G(KC_8), G(KC_9), KC_NO,
-     G(KC_S),      KC_NO,   KC_NO,    KC_LSFT, KC_NO,     G(KC_M),    G(KC_4), G(KC_5), G(KC_6), KC_NO,
-     KC_TRNS,      KC_NO,   KC_NO,      KC_NO, KC_NO,     KC_NO,      G(KC_1), G(KC_2), G(KC_3), KC_NO,
-                                  KC_TRNS, G(KC_TAB),     KC_NO,      G(KC_0)
+     S(G(KC_Q)), G(KC_W), G(KC_F), S(G(KC_P)),   KC_NO,     G(KC_0), G(KC_I), G(KC_O), G(KC_9), KC_NO,
+     G(KC_Q),      KC_NO,   KC_NO,      KC_NO, G(KC_G),     G(KC_M), G(KC_T), G(KC_N), G(KC_E), KC_NO,
+     KC_TRNS,      KC_NO,   KC_NO,    KC_NO, G(KC_TAB),     KC_NO,   G(KC_A), G(KC_R), G(KC_S), KC_NO,
+                                    KC_TRNS,     KC_NO,     KC_NO,   G(KC_SPC)
   ),
 };
